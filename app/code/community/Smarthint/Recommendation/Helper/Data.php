1713dabbce8259832165077863012c4fc01f2fb6
Renan Damarate <carlos.damarate@smarthint.com.br>
2019-07-18 12:31:21 -0300

    Release 1.1.6

--- app/code/community/Smarthint/Recommendation/Helper/Data.php
+++ app/code/community/Smarthint/Recommendation/Helper/Data.php
@@ -1,38 +1,76 @@
 <?php
 class Smarthint_Recommendation_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function send($object, $func, $url){
-        $curl = new Varien_Http_Adapter_Curl();
-        $config= array();
-        $config[CURLOPT_SSL_VERIFYPEER] = false;
-        $curl->setConfig($config);
-
-        $authA = Mage::getStoreConfig('Smarthint/identify/Token', Mage::app()->getStore());
-        $authB = Mage::getStoreConfig('Smarthint/identify/Token');
+    public function processProduct(){
+        $prodCollection = Mage::getResourceModel('catalog/product_collection')
+                        ->addAttributeToFilter("status", 1)
+                        ->addAttributeToSelect('*')
+                        ->addFinalPrice();
+ 
+        Mage::getSingleton('catalog/product_visibility')
+            ->addVisibleInCatalogFilterToCollection($prodCollection);
         
-        if ($authA != null){
-            $auth = $authA;
-        }else{
-            $auth = $authB;
+        $products = array();
+
+        foreach ($prodCollection as $product) {
+            echo $product->getTypeId() . " - " . $product->getData('entity_id') . " - " . $product->getData('name'). "<br>";
+
+            try{
+                $prd = self::getProduct($product);
+                $products = array_merge($products, $prd);
+            }
+            catch (Exception $e) {
+                echo ' Exceção capturada: ',  $e->getMessage(), "\n";
+            }
         }
-        $headers = array('Authorization: ' . $auth, "Content-Type:application/json");
-        $feed_url = $url;
-        $curl->write(Zend_Http_Client::POST, $feed_url, '1.0', $headers,json_encode($object));
-        $data = $curl->read();
-        if ($curl->getInfo(CURLINFO_HTTP_CODE) != 200) {
-            $error = new stdClass();
-            $error->status = "Error";
-            $error->function = $func;
-            $error->auth = $auth;
-            $error->object = $object;
-            $error->response = $data;
-            $JSON = json_encode($error);
-            $this->log($JSON);
+        $productsCount = count($products) / 50;
+        $productsCount = ceil($productsCount);
+
+        for ($i = 0; $i < $productsCount; $i++) {
+            $offset = $i * 50;
+            $length = $offset + 50;
+            $output = array_slice($products, $offset, $length);
+            self::send($output, "handleProduct", "https://api.smarthint.co/api/ProductList/");
+        }
+    }
+
+    public function processProductPage($page, $limit, $storeId){
+
+        $prodCollection = Mage::getResourceModel('catalog/product_collection')
+                        ->addAttributeToFilter("status", 1)
+                        ->setPageSize(intval($limit))
+                        ->setCurPage(intval($page))
+                        ->setStoreId($storeId)
+                        ->addAttributeToSelect('*')
+                        ->addFinalPrice();
+ 
+        Mage::getSingleton('catalog/product_visibility')
+            ->addVisibleInCatalogFilterToCollection($prodCollection);
+        
+        $products = array();
+
+        foreach ($prodCollection as $product) {
+            try{
+                $prd = self::getProduct($product);
+                $products = array_merge($products, $prd);
+            }
+            catch (Exception $e) {
+                echo ' Exceção capturada: ',  $e->getMessage(), "\n";
+            }
+        }
+        $productsCount = count($products) / 50;
+        $productsCount = ceil($productsCount);
+
+        for ($i = 0; $i < $productsCount; $i++) {
+            $offset = $i * 50;
+            $length = $offset + 50;
+            $output = array_slice($products, $offset, $length);
+            self::send($output, "handleProduct", "https://api.smarthint.co/api/ProductList/");
         }
     }
 
     public function getProduct($product){
-        //Simple data
+        $products = array();
         $SHProductInfo = new stdClass();
         $SHProductInfo->ProductId       = $product->getData('entity_id');
         $SHProductInfo->Title           = $product->getData('name');
@@ -45,100 +83,120 @@ class Smarthint_Recommendation_Helper_Data extends Mage_Core_Helper_Abstract
         $SHProductInfo->Availability    = "in stock";
         $SHProductInfo->Price           = (float) $product->getData('price');
         $SHProductInfo->SalePrice       = (float) $product->getData('final_price');
-        if ($SHProductInfo->Price == 0){
-            $SHProductInfo->Price = (float) $product->getPrice();
-        }
-        if ($SHProductInfo->SalePrice == 0 || 
-            $SHProductInfo->Price == (float) $SHProductInfo->SalePrice){
-            $SHProductInfo->SalePrice = (float) $product->getFinalPrice();
-        }
-        if ($product->getTypeId() == 'configurable' && $SHProductInfo->Price == 0 ){
-            $SHProductInfo->Price = (float) self::getSimpleProductPrice(null, $product->getData('entity_id'));
-            $SHProductInfo->SalePrice = (float) self::getSimpleProductFinalPrice(null, $product->getData('entity_id'));
-        
-        }
-        else if($product->getTypeId() == 'bundle'){
-            //Pode se usar o max, para obter o maior valor de combinação
-            $SHProductInfo->Price = Mage::getModel('bundle/product_price')->getTotalPrices($product,'min',0);
-        }
-        if (is_null($SHProductInfo->Price)){
-            $SHProductInfo->Price = 0;
-        }
-        if (is_null($SHProductInfo->SalePrice)){
-            $SHProductInfo->SalePrice = 0;
-        }
+        $SHProductInfo->ImageLink       = Mage::getModel('catalog/product_media_config')->getMediaUrl( $product->getSmallImage() );
+        $SHProductInfo->Categories      = array();
+        /*
+        $SHProductInfo->Installments = [
+            "Months" => 10, //Quantidade de parcelas  
+            "Amount" => 99.9 //Valor de parcelas
+        ];
+        $SHProductInfo->AdditionalTag   = "5-10";
+        */
 
-        //Description
-        if (! is_string($SHProductInfo->Description)){
-            $SHProductInfo->Description = "";
-        }
-        
-        //Stock
-        if ($product->getTypeId() == 'simple'){
-            $stockItem = Mage::getModel('cataloginventory/stock_item')
-                    ->loadByProduct($product->getData('entity_id'));
-            if ($stockItem == "0"){
-                $SHProductInfo->Availability    = "out of stock";
-            }
-        }
-        if ($product->getTypeId() == 'configurable'){
-            $stock_qty = 0;
-            $simple_ids = Mage::getResourceSingleton('catalog/product_type_configurable')->getChildrenIds($product->getId());
-            foreach ($simple_ids[0] as $simple_id) {
-                $simple_model = Mage::getModel('cataloginventory/stock_item')->loadByProduct($simple_id);
-                $stock_qty = $stock_qty + (int) $simple_model->getQty();
-            }
-            if ($stock_qty == 0) {
-                $SHProductInfo->Availability = "out of stock";
-            }
-        }
-
-        //Get primary and secondary image
-        $SHProductInfo->ImageLink = Mage::getModel('catalog/product_media_config')->getMediaUrl( $product->getSmallImage() );
-        
+        //Get secondary image
         $images = Mage::getModel('catalog/product')->load($product->getId())->getMediaGalleryImages();
-        
-        if (! is_null($images)){
-            //Primary img
-            $PrimaryLinkImage = $images->getItemByColumnValue('position', 1);
-            if (! is_null($PrimaryLinkImage) && empty($SHProductInfo->ImageLink)){
-                $SHProductInfo->ImageLink = $PrimaryLinkImage->getUrl();
-            }else if (! is_null($PrimaryLinkImage)){
-                if ($SHProductInfo->ImageLink != $PrimaryLinkImage->getUrl())
-                    $SHProductInfo->AdicionalImageLink[] = $PrimaryLinkImage->getUrl();
-            }
-            //Second img
-            $SecondLinkImage = $images->getItemByColumnValue('position', 2);
-            if (! is_null($SecondLinkImage) && empty($SHProductInfo->ImageLink)){
-                $SHProductInfo->ImageLink = $SecondLinkImage->getUrl();
-            }else if (! is_null($SecondLinkImage)){
-                $empty = "";
-                if (! is_null($PrimaryLinkImage))
-                    $empty = $PrimaryLinkImage->getUrl();
-                if ($SHProductInfo->ImageLink != $empty)
-                    $SHProductInfo->AdicionalImageLink[] = $SecondLinkImage->getUrl();
-            }
-        }
-        
-        if (empty($SHProductInfo->ImageLink)) {
+        if ($images != null){
             foreach ($images as $image){
-                $SHProductInfo->ImageLink = $image["url"];
+                if ($SHProductInfo->ImageLink != $image["url"]){
+                    $SHProductInfo->AdicionalImageLink[] = $image["url"];
+                }
                 continue;
             }
         }
-        if (empty($SHProductInfo->ImageLink)) {
-            $SHProductInfo->ImageLink = $product->getImageUrl();
+
+        //Stock
+        if (!$product->getStockItem()->getIsInStock()){
+            $SHProductInfo->Availability = "out of stock";
         }
 
-        //Get Categories
-        $cats = $product->getCategoryIds();
-        $SHProductInfo->Categories = array();
-        foreach ($cats as $category_id) {
+        //Categories
+        $categories = $product->getCategoryIds();
+        foreach ($categories as $category_id) {
             $_cat = Mage::getModel('catalog/category')->load($category_id) ;
             $SHProductInfo->Categories[] = $_cat->getName();
-        } 
+        }
         $SHProductInfo->ProductType = end($SHProductInfo->Categories);
-        return $SHProductInfo;
+        
+        //Price
+        if ($product->getTypeId() == 'bundle'){
+            $SHProductInfo->Price = Mage::getModel('bundle/product_price')->getTotalPrices($product,'min',0);
+        }
+        else if ( $product->getTypeId() == 'grouped' ){
+            $SHProductInfo->Price = (float)$this->prepareGroupedProductPrice($product);
+        }
+
+        //Variations
+        if ($product->isConfigurable()) {
+            $SHProductInfo->Variations = array();
+            $configurables = $this->getConfigurable($product, $SHProductInfo);
+            $products = array_merge($products, $configurables);
+        }else {
+            
+            array_push($products, $SHProductInfo);
+            // self::send($SHProductInfo, "handleProduct", "https://api.smarthint.co/api/Product/");
+        }
+        return $products;
+    }
+
+    public function getConfigurable($product, $SHProductInfo){
+        $products = array();
+        $atts = $product->getTypeInstance(true)->getConfigurableAttributesAsArray($product);
+        $childProducts = Mage::getModel('catalog/product_type_configurable')                        
+            ->getUsedProductCollection($product)
+            ->addAttributeToSelect('*')
+            ->addFinalPrice();
+
+
+        foreach($childProducts as $child) {
+            $childProduct                   = clone $SHProductInfo;
+            $childProduct->ItemGroupId      = $SHProductInfo->ProductId;
+            $childProduct->ProductId        = $child->getData('entity_id');
+            $childProduct->Price            = (float) $child->getData('price');
+            $childProduct->SalePrice        = (float) $child->getData('final_price');
+            $childProduct->Sku              = $child->getData('sku');
+    
+            foreach($atts as $att) {
+                $elemento = $child->getData($att['attribute_code']);
+                $opts = $att['values'];
+                $found_key = array_search($elemento, array_column($opts, 'value_index'));
+                $variations = new stdClass();
+                $variations->NameId = $att['attribute_id'];
+                $variations->Name = $att['attribute_code'];
+                $variations->ValueId = $elemento;
+                $variations->Value = $opts[$found_key]["store_label"];
+                $childProduct->Variations[] = $variations;
+            }
+
+            if (!$child->getStockItem()->getIsInStock()){
+                $childProduct->Availability = "out of stock";
+            }
+            array_push($products, $childProduct);
+            // self::send($childProduct, "handleProduct", "https://api.smarthint.co/api/Product/");
+        }
+        return $products;
+    }
+
+    public function apiProcess($val) {
+         $dateInit = array(
+            'ProductNumber' => "9999",
+            'Type' => $val
+        );
+        self::send($dateInit, "handleApiProcess", "https://api.smarthint.co/api/Process/");
+    }
+    
+    public function prepareGroupedProductPrice($groupedProduct) {
+        $aProductIds = $groupedProduct->getTypeInstance()->getChildrenIds($groupedProduct->getId());
+    
+        $prices = array();
+        foreach ($aProductIds as $ids) {
+            foreach ($ids as $id) {
+                $aProduct = Mage::getModel('catalog/product')->load($id);
+                $prices[] = $aProduct->getPriceModel()->getPrice($aProduct);
+            }
+        }
+    
+        krsort($prices);
+        return array_shift($prices);
     }
 
     public function processCategory(){
@@ -161,89 +219,8 @@ class Smarthint_Recommendation_Helper_Data extends Mage_Core_Helper_Abstract
             }
 		}
 		catch  (Exception $e) {
-			$error = new stdClass();
-            $error->OrderError = $e->getMessage();
-            $JSON = json_encode($error);
+            echo ' Categoria - Exceção capturada: ',  $e->getMessage(), "\n";
 		}
-    }
-
-    public function processProduct(){
-        $_categories = Mage::helper('catalog/category')->getStoreCategories();
-		self::getChildCategories($_categories, true);
-    }
-
-    public function getChildCategories($category, $isFirst = false) {
-
-        $children = $isFirst ? $category : $category->getChildren();
-
-        foreach ($children as $child) {
-            $category = Mage::getModel('catalog/category')->load($child->getId());
-
-            $prodCollection = Mage::getResourceModel('catalog/product_collection')
-                        ->addCategoryFilter($category)
-                        ->addAttributeToSelect('*');
-             
-            Mage::getSingleton('catalog/product_status')
-                ->addVisibleFilterToCollection($prodCollection);    
-                
-            Mage::getSingleton('catalog/product_visibility')
-                ->addVisibleInCatalogFilterToCollection($prodCollection);
-
-            // Mage::getSingleton('cataloginventory/stock')
-            //     ->addInStockFilterToCollection($prodCollection);
-
-            foreach ($prodCollection as $product) {
-                $SHProductInfo = self::getProduct($product);
-                self::send($SHProductInfo, "handleProduct", "https://api.smarthint.co/api/Product/");
-            }
-
-            self::getChildCategories($child);
-        }
-    }
-    
-    public function getSimpleProductPrice($qty=null, $cfgId)
-    {
-        $price = 0;
-        $childProducts = Mage::getModel('catalog/product_type_configurable')
-                            ->getChildrenIds($cfgId);
-
-        foreach($childProducts as $childId) {
-            foreach($childId as $child) 
-            {
-                $product = Mage::getModel('catalog/product')->load($child);
-                $childPrice = (float) $product->getData('price');
-
-                if ($childPrice == 0){
-                    $price = $childPrice; 
-                }
-                else if ($price < $childPrice){
-                    $price = $childPrice; 
-                }
-            }
-        }
-        return $price;
-    }
-
-    public function getSimpleProductFinalPrice($qty=null, $cfgId)
-    {
-        $price = 0;
-        $childProducts = Mage::getModel('catalog/product_type_configurable')
-                            ->getChildrenIds($cfgId);
-
-        foreach($childProducts as $childId) {
-            foreach($childId as $child) 
-            {
-                $product = Mage::getModel('catalog/product')->load($child);
-                $childPrice = (float) $product->getData('final_price');
-                if ($childPrice == 0){
-                    $price = $childPrice; 
-                }
-                else if ($price > $childPrice){
-                    $price = $childPrice; 
-                }
-            }
-        }
-        return $price;
     }
     
     public function log($JSON){
@@ -256,5 +233,35 @@ class Smarthint_Recommendation_Helper_Data extends Mage_Core_Helper_Abstract
         $curl->setOptions($opts);
         $curl->write(Zend_Http_Client::POST, $URL, '1.0', $header,addslashes($JSON));
         $data = $curl->read();
+    }
+
+    public function send($object, $func, $url){
+        $curl = new Varien_Http_Adapter_Curl();
+        $config= array();
+        $config[CURLOPT_SSL_VERIFYPEER] = false;
+        $curl->setConfig($config);
+
+        $authA = Mage::getStoreConfig('Smarthint/identify/Token', Mage::app()->getStore());
+        $authB = Mage::getStoreConfig('Smarthint/identify/Token');
+        
+        if ($authA != null){
+            $auth = $authA;
+        }else{
+            $auth = $authB;
+        }
+        $myHeaders = array('Authorization: ' . $auth, "Content-Type:application/json");
+        $feed_url = $url;
+        $curl->write(Zend_Http_Client::POST, $feed_url, '1.0', $myHeaders,json_encode($object));
+        $data = $curl->read();
+        if ($curl->getInfo(CURLINFO_HTTP_CODE) != 200) {
+            $error = new stdClass();
+            $error->status = "Error";
+            $error->function = $func;
+            $error->auth = $auth;
+            $error->object = $object;
+            $error->response = $data;
+            $JSON = json_encode($error);
+            $this->log($JSON);
+        }
     }
 }
--- app/code/community/Smarthint/Recommendation/controllers/IndexController.php
+++ app/code/community/Smarthint/Recommendation/controllers/IndexController.php
@@ -3,9 +3,61 @@ class Smarthint_Recommendation_IndexController extends Mage_Core_Controller_Fron
 
     public function indexAction()
     {
-        //Processa produtos
+        set_time_limit(0);
+        $process = new stdClass();
+        $process->status = "Atualização";
+        $process->version = "1.1.5";
+        $process->domain = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+        $process->email  = Mage::getStoreConfig('smarthint/identify/email', $observer->store);
+        $JSON = json_encode($process);
+
+        Mage::helper('smarthint')->log($JSON);
+        Mage::helper('smarthint')->apiProcess(1);
         Mage::helper('smarthint')->processProduct();
-        //Processa categoria
         Mage::helper('smarthint')->processCategory();
+        Mage::helper('smarthint')->apiProcess(2);
+    }
+
+    public function versionAction()
+    {
+        $version = new stdClass();
+        $version->version = "1.1.5";
+        $this->getResponse()->clearHeaders()->setHeader('Content-type','application/json',true);
+        $this->getResponse()->setBody(json_encode($version));
+    }
+
+    public function productsAction()
+    {
+        $storeId = Mage::app()->getStore()->getStoreId();
+        $ids = Mage::getResourceModel('catalog/product_collection')
+                        ->addAttributeToFilter("status", 1)
+                        ->setStoreId($storeId)
+                        ->getAllIds();
+        
+        $page = $this->getRequest()->getParam('page');
+        $limit = $this->getRequest()->getParam('limit');
+        $hasnext = ((intval($page) * intval($limit)) < count($ids));
+
+        if ($hasnext){
+
+            Mage::helper('smarthint')->processProductPage($page, $limit, $storeId);
+
+            $page = intval($page) + 1;
+            $url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+                "/smarthint/index/products" .
+                "?page=" . $page . 
+                "&limit=" .$limit;
+
+            $link = new stdClass();
+            $link->url = $url;
+            $this->getResponse()->clearHeaders()->setHeader('Content-type','application/json',true);
+            $this->getResponse()->setBody(json_encode($link));
+        }
+        else{
+            $link = new stdClass();
+            $link->url = "";
+            $this->getResponse()->clearHeaders()->setHeader('Content-type','application/json',true);
+            $this->getResponse()->setBody(json_encode($link));
+        }
     }
 }
